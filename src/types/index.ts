db12b578a6fbf2af589d24879c0f44c086b2eadc
beyondwebco-lgtm <beyondwebco@gmail.com>
2026-07-28 00:45:07 +0530

    Fix BetRow type missing properties

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,12 @@ export interface BetRow {
   serialNo: number;
   clientName: string;
   horse: string;
+  race: string;
   betType: string;
   stake: number | null;
   odds: number | null;
+  deductionPercent: number | null;
+  netOdds: number | null;
   returnAmt: number | null;
   profit: number | null;
   remarks: string;
